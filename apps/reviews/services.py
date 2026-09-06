0ed5530158d0a93031f0cab869e4256db5bb12b5
TarasikPy <interestingall24@gmail.com>
2026-09-06 08:36:56 +0000

    feat(reviews): notify author and subscribers via email on new review

--- apps/reviews/services.py
+++ apps/reviews/services.py
@@ -44,3 +44,54 @@ def export_reviews_to_json(queryset) -> HttpResponse:
     response = HttpResponse(json_data, content_type='application/json; charset=utf-8')
     response['Content-Disposition'] = 'attachment; filename="reviews.json"'
     return response
+
+
+def notify_author_and_subscribers_on_new_review(review) -> None:
+    from django.conf import settings
+    from django.core.mail import send_mail
+
+    location = review.location
+    from_email = getattr(settings, 'DEFAULT_FROM_EMAIL', 'noreply@locations.local')
+    author = location.author
+
+    if author and author.email and author.id != review.author_id:
+        subject = f'New review for your location "{location.name}"'
+        message = (
+            f'Hi {author.username},\n\n'
+            f'User "{review.author.username}" left a new review for your location "{location.name}".\n'
+            f'Rating: {review.rating}/5\n'
+            f'Comment: {review.text}\n'
+        )
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=[author.email],
+            fail_silently=True,
+        )
+
+    subscriptions = (
+        location.subscriptions
+        .select_related('user')
+        .exclude(user_id=review.author_id)
+    )
+    if author:
+        subscriptions = subscriptions.exclude(user_id=author.id)
+
+    for sub in subscriptions:
+        if sub.user and sub.user.email:
+            subject = f'New review for subscribed location "{location.name}"'
+            message = (
+                f'Hi {sub.user.username},\n\n'
+                f'A new review has been posted on subscribed location "{location.name}".\n'
+                f'Reviewer: {review.author.username}\n'
+                f'Rating: {review.rating}/5\n'
+                f'Comment: {review.text}\n'
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=from_email,
+                recipient_list=[sub.user.email],
+                fail_silently=True,
+            )
--- apps/reviews/signals.py
+++ apps/reviews/signals.py
@@ -2,10 +2,18 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from apps.locations.services import invalidate_locations_cache
 from apps.reviews.models import Review, ReviewReaction
+from apps.reviews.services import notify_author_and_subscribers_on_new_review
 
 
-@receiver([post_save, post_delete], sender=Review)
-def on_review_changed(sender, instance, **kwargs):
+@receiver(post_save, sender=Review)
+def on_review_saved(sender, instance, created, **kwargs):
+    invalidate_locations_cache()
+    if created:
+        notify_author_and_subscribers_on_new_review(instance)
+
+
+@receiver(post_delete, sender=Review)
+def on_review_deleted(sender, instance, **kwargs):
     invalidate_locations_cache()
 
 
